21ca123825ec7775312868541d6d84c0706d2c0a
newbie053 <dipto30901@gmail.com>
2025-10-09 12:24:24 +0600

    add daily DOM practice

---
+++ script.js
@@ -0,0 +1,72 @@
+//DOM Manipulation
+
+
+// const title = document.getElementById('main-heading');
+// console.log(title.textContent);
+
+// const listItem = document.getElementsByClassName('list-items');
+// console.log(listItem);
+// for(let item of listItem){
+//     console.log(item.textContent);
+// }
+
+// const listItem2 = document.getElementsByTagName('li');
+// console.log(listItem2);
+// for(let item of listItem2){
+//     console.log(item.textContent);
+// }
+
+
+// const container = document.querySelector('div');
+// console.log(container);
+// const container2 = document.querySelectorAll('div');
+// console.log(container2)
+
+
+// const  title = document.querySelector('#main-heading');
+// title.style.color= 'red';
+
+// title.style.padding = '20px';
+// title.style.border = '2px ssolid black';
+// title.style.background = 'yellow';
+// console.log(title);
+
+
+
+// styling element
+
+// const listItem = document.querySelectorAll('.list-items');
+// for (i =0;i<listItem.length;i++){
+// listItem[i].style.color='aqua';
+// listItem[i].style.fontSize = '20px';
+// }
+// for(i of listItem){
+//     i.style.padding = '20px';
+// }
+
+//creating element
+
+// const ul = document.querySelector('ul');
+// const li = document.createElement('li');
+// li.innerText='DC';
+// ul.append(li);
+// li.setAttribute('id','main-heading');
+// li.removeAttribute('id');
+// li.remove();
+
+// const firstListItem = document.querySelector('.list-items');
+// console.log(firstListItem.innerText);
+
+//Parent Node Traversal
+
+let ul = document.querySelector('ul');
+console.log(ul);
+console.log(ul.parentNode.parentNode);
+console.log(ul.parentElement.parentElement);
+
+
+
+
+
+
+
