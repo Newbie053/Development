f6cd9a0e72ea7ba1cb8944f1433279e0e49187ea
newbie053 <dipto30901@gmail.com>
2025-10-09 15:56:16 +0600

    DOM overall

--- Dom_Maro_Dom/script.js
+++ Dom_Maro_Dom/script.js
@@ -100,6 +100,75 @@ box3.addEventListener("mouseout", function() {
 
 
 
+//Event Propagation
+
+//Event Delegation
+
+
+// document.querySelector('#football').addEventListener("click",
+//     function(e){
+//         console.log('football is clicked!');
+//         const target =e.target;
+//         if(target.matches('li')){
+//             target.style.backgroundColor = 'yellow';
+//         }
+//     }
+// )
+// document.querySelector('#basketball').addEventListener("click",
+//     function(e){
+//         console.log('basketball is clicked!');
+//                 const target =e.target;
+//         if(target.matches('li')){
+//             target.style.backgroundColor = 'yellow';
+//         }
+//     }
+// )
+// document.querySelector('#boxing').addEventListener("click",
+//     function(e){
+//         console.log('boxing is clicked!');
+//                 const target =e.target;
+//         if(target.matches('li')){
+//             target.style.backgroundColor = 'yellow';
+//         }
+//     }
+// )
+// document.querySelector('#tennis').addEventListener("click",
+//     function(e){
+//         console.log('tennis is clicked!');
+//                 const target =e.target;
+//         if(target.matches('li')){
+//             target.style.backgroundColor = 'yellow';
+//         }
+//     }
+// )
+// document.querySelector('#golf').addEventListener("click",
+//     function(e){
+//         console.log('golf is clicked!');
+//                 const target =e.target;
+//         if(target.matches('li')){
+//             target.style.backgroundColor = 'yellow';
+//         }
+//     }
+// )
+
+
+
+document.querySelector('#sports').addEventListener("click",
+    function(e){
+        console.log(e.target.id+ ' is clicked!');
+        const target =e.target;
+        if(target.matches('li')){
+            target.style.backgroundColor = 'yellow';
+        }
+    }
+);
+
+const sports = document.querySelector('#sports');
+const newSport = document.createElement('li');
+
+newSport.innerText = 'rugby';
+newSport.setAttribute('id','rugby');
+sports.appendChild(newSport);
 
 
 
